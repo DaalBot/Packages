16d09ad2f4851d6aa645fafe9af0603c98e3d418
NotPiny <103984634+NotPiny@users.noreply.github.com>
2025-06-09 12:58:35 +0100

    Update base route for DBAPI class, bump to 1.0.0

--- api/src/classes.ts
+++ api/src/classes.ts
@@ -2,12 +2,9 @@ import type { DBAPIOptions } from "./types.js";
 
 export class DBAPI {
     constructor(options: DBAPIOptions) {
-        this.base = 'http://localhost:3000'; // TODO: SWITCH TO api.daalbot.xyz BEFORE 1.0.0
+        this.base = 'https://api.daalbot.xyz';
         this.auth = options.auth;
         this.guildId = options.guildId;
-
-        if (typeof window != 'undefined') return; // We are about to use a ton of node code, so obviously we don't want to run this in the browser
-        if (process && process.env.DAALBOT_API_WRAPPER_TEST_ENABLE=='1') this.base = 'http://localhost:3000'; // This is for developing the wrapper for features that are not yet in the API
     }
 
     auth: DBAPIOptions['auth'];
